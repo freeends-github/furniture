9735b05ec9aa0f03879a5a68c20075d08fa63b1a
Fatbardh <fatbardhemini5@gmail.com>
2020-02-29 01:03:47 -0800

    Making the project responsive

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 
 // Set the hours you want at 'a' ideally smaller than number 24
-var a = 12;
+var a = 15;
 var countdown = a*60*60;
 let count = 0;
 // Update the count down every 1 second
@@ -15,7 +15,7 @@ let count = 0;
     var seconds = Math.floor((distance % 60));
   
      if(hours<10){
-       hours="0"+hours;
+       hours="0" + hours;
        document.getElementById("hours").innerHTML = hours;
      }
      if(minutes<10){
@@ -38,3 +38,54 @@ let count = 0;
       document.getElementById("seconds").innerHTML = "00";
     }
   }, 1000);
+
+  // var search = document.querySelector(".search_div");
+  // var input = document.querySelector(".input_div input");
+  // var header = document.querySelector(".list_div");
+
+  // search.addEventListener("click",function(){
+    
+  //   // input.style.display = "block";
+  //  if(input.style.width = "1px"){
+  //   input.style.width = "300px";
+  //   input.style.padding = "10px 20px";
+  //   input.style.border = "1px solid gray";
+  //   header.style.display = "none";
+  //  }
+  //  else{
+  //    input.style.width = "0";
+  //    input.style.padding = "0";
+  //    input.style.border = "none";
+  //    header.style.display = "flex";
+  //  }
+  // });
+
+
+  // var number = document.querySelector(".topright>p");
+  // var cart = document.querySelectorAll(".cart-market button");
+
+  // cart.addEventListener("click",function(){
+  //   cart.style.display = "none";
+  //   console.log("a");
+  // });
+
+var email = document.querySelector(".email_div input");
+var button = document.querySelector(".email_div button");
+
+button.addEventListener("click",function(){
+  email.value = ""
+  console.log("button");
+});
+
+
+var menu = document.querySelector(".hamburger_menu");
+var menuclass = document.querySelector(".menu_class");
+var x = document.querySelector(".menu_class i")
+
+menu.addEventListener("click",function(){
+  menuclass.style.display = "block";
+});
+
+x.addEventListener("click",function(){
+  menuclass.style.display = "none";
+});
